a339a4a1c2d5b77c459c3b7555c610255d7a35b9
olavodalberto <olavodalberto921@gmail.com>
2025-01-12 09:38:33 -0300

    Add login page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App h-screen flex items-center justify-center bg-gray-100">
+      <div className="border-box flex items-center justify-center h-[60%] w-[40%] bg-gray-50 shadow-lg rounded-md">
+        <div className="flex flex-col items-center w-full">
+          <form className="loginForm bg-gray-100 p-6 rounded-md shadow-md w-full">
+            <div className="flex flex-col gap-4">
+              <div className="inline-block">
+                <label htmlFor="loginEmail">E-mail:</label>
+              </div>
+              <div className="inline-block">
+                <input 
+                  type="text" 
+                  name="loginEmail" 
+                  placeholder="Insira seu email" 
+                  className="border border-gray-300 p-2 rounded w-full"
+                />
+              </div>
+              <div className="inline-block">
+                <label htmlFor="loginPassword">Senha:</label>
+              </div>
+              <div className="inline-block">
+                <input 
+                  type="password" 
+                  name="loginPassword" 
+                  placeholder="Insira a senha" 
+                  className="border border-gray-300 p-2 rounded w-full"
+                />
+              </div>
+              <button 
+                type="submit" 
+                className="bg-black text-white p-2 rounded hover:bg-gray-500 w-full"
+              >
+                Sign In
+              </button>
+              <div className="mt-4">
+            <a href="#" className="text-black hover:underline">
+              Forgot password?
+            </a>
+            </div>
+            </div>
+          </form>
+          
+        </div>
+      </div>
     </div>
   );
 }
