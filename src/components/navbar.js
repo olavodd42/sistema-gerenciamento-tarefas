8e9f030ac1ffdc34799d504e78430c89e00ad79d
olavodalberto <olavodalberto921@gmail.com>
2025-01-12 22:07:50 -0300

    Add navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars, faHouse, faThumbTack, faThumbTackSlash } from '@fortawesome/free-solid-svg-icons';
+
+const items = [
+  { name: "Home", url: "/home", icon: faHouse },
+  { name: "Itens pendentes", url: "#", icon: faThumbTack },
+  { name: "Itens concluidos", url: "#", icon: faThumbTackSlash },
+];
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <a className="navbar-brand" href="/">Minha App</a>
+    <nav className="h-screen w-[4%] bg-gray-800 text-white fixed">
+      <div className="flex flex-col items-center p-4">
+        <FontAwesomeIcon icon={faBars} className="mb-4" />
+        <ul className="w-full">
+          {items.map((item, index) => (
+            <li key={index} className="w-full mb-4">
+              <a className="flex flex-col items-center p-2 hover:bg-gray-700 rounded" href={item.url}>
+                <FontAwesomeIcon icon={item.icon} className="mb-2" />
+                <span className="text-xs text-center">{item.name}</span>
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
--- src/home.js
+++ src/home.js
@@ -1,12 +1,15 @@
+// filepath: /c:/Users/olavo/OneDrive/Documentos/sist_gerenciamento_tarefas/src/home.js
 import React from "react";
 import Navbar from "./components/navbar";
 
 function Home() {
   return (
-    <>
+    <div className="flex">
       <Navbar />
-      <div className="p-4 text-center">Bem-vindo à página inicial!</div>
-    </>
+      <div className="ml-64 p-4 text-center w-full">
+        Bem-vindo à página inicial!
+      </div>
+    </div>
   );
 }
 
