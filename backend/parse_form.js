d3d03a3f02fa229c61127f7d101f3993dece20f8
olavodalberto <olavodalberto921@gmail.com>
2025-01-14 16:58:43 -0300

    Add backend init

---
+++ backend/parse_form.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+const cors = require('cors');
+const axios = require('axios');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', async (req, res) => {
+  const id = randomBytes(4).toString('hex');
+  const { taskName, taskDescription, taskDate, taskTime, endTime } = req.body;
+
+  posts[id] = {
+    id,
+    taskName,
+    taskDescription,
+    taskDate,
+    taskTime,
+    endTime
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.post('/events',(req,res)=>{
+  console.log('res event',req.body.type)
+  res.send({});
+})
+
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
