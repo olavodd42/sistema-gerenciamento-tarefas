3213c944e3f46f499b1ce710ed23bf08152581b9
olavodalberto <olavodalberto921@gmail.com>
2025-01-14 13:32:30 -0300

    Add checkbox and edit tab

--- src/components/edit_tab.js
+++ src/components/edit_tab.js
@@ -2,35 +2,87 @@ import React from "react";
 
 function EditTab() {
   return (
-    <form className="h-[40%] z-20 bg-gray-600 p-4 rounded-md shadow-md w-[40%] absolute bottom-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 grid gap-6 mb-6 md:grid-cols-2">
+    <form className="absolute top-[20%] right-[3%] z-50 bg-gray-500 p-4 rounded-md shadow-md w-[40%] grid gap-6 mb-6 md:grid-cols-2">
       <div>
-        <label htmlFor="taskName" className="mb-2 text-sm font-medium text-gray-900 dark:text-white">Nome da tarefa:</label>
-        <input type="text" id="taskName" name="taskName" className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 h-7 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+        <label htmlFor="taskName" className="mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Nome da tarefa:
+        </label>
+        <input
+          type="text"
+          id="taskName"
+          name="taskName"
+          className="bg-gray-100 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 h-7 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          required
+        />
       </div>
       <div>
-        <label htmlFor="taskDescription" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Descrição da tarefa:</label>
-        <textarea id="taskDescription" name="textDescription" rows="4" className="block p-2.5 w-full h-12 text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+        <label htmlFor="taskDescription" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Descrição da tarefa:
+        </label>
+        <textarea
+          id="taskDescription"
+          name="textDescription"
+          rows="4"
+          className="block p-2.5 w-full h-12 text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Write your thoughts here..."
+        ></textarea>
       </div>
       <div>
-        <label htmlFor="taskDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Data da tarefa:</label>
+        <label htmlFor="taskDate" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Data da tarefa:
+        </label>
         <div className="relative max-w-sm">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z"/>
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path d="M20 4a2 2 0 0 0-2-2h-2V1a1 1 0 0 0-2 0v1h-3V1a1 1 0 0 0-2 0v1H6V1a1 1 0 0 0-2 0v1H2a2 2 0 0 0-2 2v2h20V4ZM0 18a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V8H0v10Zm5-8h10a1 1 0 0 1 0 2H5a1 1 0 0 1 0-2Z" />
             </svg>
           </div>
-          <input datepicker id="taskDate" name="taskDate" type="text" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Select date"/>
+          <input
+            datepicker
+            id="taskDate"
+            name="taskDate"
+            type="text"
+            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Select date"
+          />
         </div>
       </div>
       <div>
-        <label htmlFor="taskTime" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Hora da tarefa:</label>
+        <label htmlFor="taskTime" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Hora da tarefa:
+        </label>
         <div className="relative">
           <div className="absolute inset-y-0 end-0 top-0 flex items-center pe-3.5 pointer-events-none">
-            <svg className="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
-              <path fillRule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z" clipRule="evenodd"/>
+            <svg
+              className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path
+                fillRule="evenodd"
+                d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v4a1 1 0 0 0 .293.707l3 3a1 1 0 0 0 1.414-1.414L13 11.586V8Z"
+                clipRule="evenodd"
+              />
             </svg>
           </div>
-          <input type="taskTime" id="taskTime" className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" min="09:00" max="18:00" value="00:00" required />
+          <input
+            type="time"
+            id="taskTime"
+            name="taskTime"
+            className="bg-gray-50 border leading-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            min="09:00"
+            max="18:00"
+            defaultValue="00:00"
+            required
+          />
         </div>
       </div>
       <div>
@@ -38,7 +90,12 @@ function EditTab() {
         <input type="time" id="endTime" name="endTime" required />
       </div>
       <div>
-        <button type="submit" className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2">Criar</button>
+        <button
+          type="submit"
+          className="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+        >
+          Criar
+        </button>
       </div>
     </form>
   );
--- src/home.js
+++ src/home.js
@@ -22,31 +22,86 @@ function Home() {
     });
   };
 
+  const confirmDelete = () => {
+      //confirm("Tem certeza que deseja deletar?");
+  };
+
   return (
     <div className="flex">
       <Navbar />
 
-      <div className="relative ml-[4%] p-4 text-center bg-gray-600 box-border h-screen flex-grow flex flex-col justify-start">
+      <div className="relative ml-[4%] overflow-y-auto p-4 h-screen text-center bg-gray-600 box-border h-screen flex-grow flex flex-col">
         <CreateButton onClick={toggleTab} />
         {isTabVisible && <CreateTab />}
-        <div className={`bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative ${isTabVisible ? "mt-80" : ""}`}>
-          Conteúdo 1
-          <EditButton onClick={() => toggleEditTab(0)} />
-          <DeleteButton />
-          {editTabsVisibility[0] && <EditTab />}
-        </div>
-        <div className="bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative">
-          Conteúdo 2
-          <EditButton onClick={() => toggleEditTab(1)} />
-          <DeleteButton />
-          {editTabsVisibility[1] && <EditTab />}
-        </div>
-        <div className="bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative">
-          Conteúdo 3
-          <EditButton onClick={() => toggleEditTab(2)} />
-          <DeleteButton />
-          {editTabsVisibility[2] && <EditTab />}
-        </div>
+
+        {/* Conteúdo 1 */}
+        
+          <div className={`relative bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 ${editTabsVisibility[0] ? "mb-60" : ""}`}>
+            Conteúdo 1
+            <EditButton onClick={() => toggleEditTab(0)} />
+            <DeleteButton onClick={confirmDelete}/>
+            {editTabsVisibility[0] && <EditTab />}
+            <form className="inline flex items-center justify-center space-x-2">
+            <input
+              type="checkbox"
+              id="concluido2"
+              name="concluido"
+              value="concluido"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="concluido2"
+              className="text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            >
+              Concluido
+            </label>
+          </form>
+          </div>
+
+        {/* Conteúdo 2 */}
+          <div className={`relative bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 ${editTabsVisibility[1] ? "mb-60" : ""}`} >
+            Conteúdo 2
+            <EditButton onClick={() => toggleEditTab(1)} />
+            <DeleteButton />
+            {editTabsVisibility[1] && <EditTab />}
+            <form className="inline flex items-center justify-center space-x-2">
+            <input
+              type="checkbox"
+              id="concluido2"
+              name="concluido"
+              value="concluido"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="concluido2"
+              className="text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            >
+              Concluido
+            </label>
+          </form>
+          </div>
+
+          <div className={`relative bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 ${editTabsVisibility[2] ? "mb-60" : ""}`} >
+            Conteúdo 3
+            <EditButton onClick={() => toggleEditTab(2)} />
+            <DeleteButton />
+            {editTabsVisibility[2] && <EditTab />}
+            <form className="inline flex items-center justify-center space-x-2">
+            <input
+              type="checkbox"
+              id="concluido2"
+              name="concluido"
+              value="concluido"
+              className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            />
+            <label
+              htmlFor="concluido2"
+              className="text-blue-600 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+            >
+              Concluido
+            </label>
+          </form>
+          </div>
       </div>
     </div>
   );
