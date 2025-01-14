6abae01704347cd4ee3102bcfe0a7cd4402a35ed
olavodalberto <olavodalberto921@gmail.com>
2025-01-14 09:14:36 -0300

    add edit tab

--- src/components/edit_button.js
+++ src/components/edit_button.js
@@ -2,13 +2,13 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPen } from "@fortawesome/free-solid-svg-icons";
 
-function EditButton ({ onclick }) {
+function EditButton({ onClick }) {
   return (
     <div className="inline-block ml-2 absolute top-2 right-2">
       <FontAwesomeIcon
-        icon={faPen}s
+        icon={faPen}
         className="cursor-pointer text-white"
-        //onClick={onClick}
+        onClick={onClick}
       />
     </div>
   );
--- src/home.js
+++ src/home.js
@@ -1,16 +1,25 @@
-// filepath: /c:/Users/olavo/OneDrive/Documentos/sist_gerenciamento_tarefas/src/home.js
 import React, { useState } from "react";
 import Navbar from "./components/navbar";
 import CreateButton from "./components/create_button";
 import EditButton from "./components/edit_button";
 import DeleteButton from "./components/delete_button";
 import CreateTab from "./components/create_tab";
+import EditTab from "./components/edit_tab";
 
 function Home() {
   const [isTabVisible, setIsTabVisible] = useState(false);
+  const [editTabsVisibility, setEditTabsVisibility] = useState([false, false, false]);
 
   const toggleTab = () => {
     setIsTabVisible(!isTabVisible);
+  };
+
+  const toggleEditTab = (index) => {
+    setEditTabsVisibility((prevVisibility) => {
+      const newVisibility = [...prevVisibility];
+      newVisibility[index] = !newVisibility[index];
+      return newVisibility;
+    });
   };
 
   return (
@@ -19,24 +28,24 @@ function Home() {
 
       <div className="relative ml-[4%] p-4 text-center bg-gray-600 box-border h-screen flex-grow flex flex-col justify-start">
         <CreateButton onClick={toggleTab} />
-        {isTabVisible && (
-          <CreateTab />
-        )}
+        {isTabVisible && <CreateTab />}
         <div className={`bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative ${isTabVisible ? "mt-80" : ""}`}>
           Conteúdo 1
-          <EditButton />
+          <EditButton onClick={() => toggleEditTab(0)} />
           <DeleteButton />
-
+          {editTabsVisibility[0] && <EditTab />}
         </div>
         <div className="bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative">
           Conteúdo 2
-          <EditButton />
+          <EditButton onClick={() => toggleEditTab(1)} />
           <DeleteButton />
+          {editTabsVisibility[1] && <EditTab />}
         </div>
         <div className="bg-gray-400 p-6 rounded-md shadow-md w-[97%] mb-4 relative">
           Conteúdo 3
-          <EditButton />
+          <EditButton onClick={() => toggleEditTab(2)} />
           <DeleteButton />
+          {editTabsVisibility[2] && <EditTab />}
         </div>
       </div>
     </div>
